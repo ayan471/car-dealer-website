e97aa546f677f8bb4ab18bec5361d67d14a92bc2
ayan471 <pakhiraayan@gmail.com>
2025-04-23 23:22:23 +0530

    changed prisma version

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   compress: true,
   reactStrictMode: true,
   compiler: {
-    removeConsole: process.env.NODE_ENV === "development",
+    removeConsole: process.env.NODE_ENV === "production",
   },
   experimental: {
     authInterrupts: true,

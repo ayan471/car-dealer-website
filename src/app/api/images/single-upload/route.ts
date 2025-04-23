a20e7f81db74932da93bca36fdb531578894a3e9
ayan471 <pakhiraayan@gmail.com>
2025-04-23 23:49:12 +0530

    mfixing build error

--- src/app/api/images/single-upload/route.ts
+++ src/app/api/images/single-upload/route.ts
@@ -9,7 +9,7 @@ import { forbidden } from "next/navigation";
 import { NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 
-export const maxDuration = 300;
+export const maxDuration = 60;
 
 export const POST = auth(async (req) => {
   if (!req.auth) forbidden();
